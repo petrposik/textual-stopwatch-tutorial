c9d31d30b16bf7308cd3a78284c42b91e4522a9f
Petr Pošík <petr.posik@gmail.com>
2023-07-27 15:21:00 +0200

    Add StopWatch widget

--- stopwatch.py
+++ stopwatch.py
@@ -1,14 +1,29 @@
 from textual.app import App, ComposeResult
-from textual.widgets import Footer, Header, Button
+from textual.widgets import Footer, Header, Button, Static
+
+
+class TimeDisplay(Static):
+    """Custom time display widget"""
+
+    pass
+
+
+class StopWatch(Static):
+    """Custom Stopwatch widget"""
+
+    def compose(self) -> ComposeResult:
+        yield Button("Start", id="start", variant="success")
+        yield Button("Stop", id="stop", variant="error")
+        yield Button("Reset", id="reset")
+        yield TimeDisplay("00:00:00.00", id="time_display")
 
 
 class StopWatchApp(App):
     BINDINGS = [("d", "toggle_dark_mode", "Toggle dark mode")]
 
     def compose(self) -> ComposeResult:
-        yield Header(name="Stopwatch", show_clock=True)
-        yield Button("Start", name="start")
-        yield Button("Stop", name="stop")
+        yield Header(show_clock=True)
+        yield StopWatch()
         yield Footer()
 
     def action_toggle_dark_mode(self):
