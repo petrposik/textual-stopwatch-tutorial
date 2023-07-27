ae37c9a74e7674ff96fc97b0fd5cf595199316fa
Petr Pošík <petr.posik@gmail.com>
2023-07-27 13:12:48 +0200

    Add dark mode toggling

--- stopwatch.py
+++ stopwatch.py
@@ -3,11 +3,17 @@ from textual.widgets import Footer, Header, Button
 
 
 class StopWatchApp(App):
+    BINDINGS = [("d", "toggle_dark_mode", "Toggle dark mode")]
+
     def compose(self) -> ComposeResult:
         yield Header(name="Stopwatch", show_clock=True)
         yield Button("Start", name="start")
         yield Button("Stop", name="stop")
         yield Footer()
+
+    def action_toggle_dark_mode(self):
+        """Toggle dark mode on or off."""
+        self.dark = not self.dark
 
 
 if __name__ == "__main__":
